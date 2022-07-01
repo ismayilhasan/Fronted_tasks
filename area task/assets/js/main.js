5606b1b83ad1b3cd160fa5357688e5b9c0fa5358
unknown <ismayilrha@code.edu.az>
2022-07-01 14:16:14 +0400

    Homework

---
+++ area task/assets/js/main.js
@@ -0,0 +1,16 @@
+// for square
+
+var a = 7
+
+var area = Math.pow(a,2);
+
+console.log(area)
+
+
+// for circle  
+
+var r = 5
+
+var circle_area = 2 * Math.PI * Math.pow(r,2)
+
+console.log(Math.round(circle_area))
