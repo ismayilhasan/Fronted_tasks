d7ee8b2dff34cf9fe407b0b5dbfd102425941c4a
unknown <ismayilrha@code.edu.az>
2022-07-05 14:04:39 +0400

    task

--- group select/js/app.js
+++ group select/js/app.js
@@ -3,12 +3,14 @@ function sum(){
     var input = document.getElementById('group').value;
     var day =  input.slice(-3,-2);
     
-    if(input == 1)
+    if(day == 1)
     {
         document.write(`your group ${input} and morning your lesson sAart`)
         document.body.style.backgroundColor = 'yellow'
+        // document.getElementById('span').innerHTML(input)
+        // document.getElementById('span').style.backgroundColor = 'yellow'
     }
-    else if(input == 2)
+    else if(day == 2)
     {
         document.write(`your group ${input} and afternoon your lesson sAart`)
         document.body.style.backgroundColor = 'red'
